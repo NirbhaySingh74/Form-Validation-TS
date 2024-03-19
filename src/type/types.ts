77e28b9b1854d7403a5c3446752f44db9e246fae
Nirbhay <nirbhaysingh943012@gmail.com>
2024-03-19 08:26:08 +0530

    reducelengthof-pass

--- src/type/types.ts
+++ src/type/types.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const signUpSchema = z
   .object({
     email: z.string().email(),
-    password: z.string().min(10, "password must be at least 10 characters"),
+    password: z.string().min(7, "password must be at least 7 characters"),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
